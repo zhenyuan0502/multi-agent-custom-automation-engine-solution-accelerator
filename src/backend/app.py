56b3ed7ac266e7869d66efc284596d5958a7fbb0
UtkarshMishra-Microsoft <v-utkamishra@microsoft.com>
2025-02-09 14:18:57 +0530

    appfixes

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 # Add the parent directory (the one that contains the "src" folder) to sys.path.
-# This allows absolute imports such as "from src.backend.middleware.health_check" to work.
+# This allows absolute imports such as "from src.backend.middleware.health_check" to work
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
 import asyncio
 import logging
