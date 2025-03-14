f70405956a99726480143c09231a4cca62a1e464
Kanchan-Microsoft <v-knagshetti@microsoft.com>
2025-03-14 10:21:59 +0530

    Update task.js

--- src/frontend/wwwroot/task/task.js
+++ src/frontend/wwwroot/task/task.js
@@ -252,29 +252,17 @@
           taskMessageTextareaElement.style.backgroundColor = "#efefef";
           taskMessageTextareaElement.style.cursor = 'not-allowed';
         } 
-        // else {
-        //   taskMessageTextareaElement.removeAttribute('disabled')
-        //   taskMessageTextareaElement.style.backgroundColor = "white"
-        //   taskMessageTextareaElement.style.cursor = '';
-        // }
+        
         if(isHumanClarificationRequestNull && taskMessageAddButton){
           taskMessageAddButton.setAttribute('disabled', true)
           taskMessageAddButton.style.cursor = 'not-allowed';
         } 
-        // else {
-        //   taskMessageAddButton.removeAttribute('disabled')
-        //   taskMessageAddButton.style.cursor = 'pointer';
-        // }
-
+        
         if(isHumanClarificationRequestNull && textInputContainer[0]){ 
           textInputContainer[0].style.backgroundColor = '#efefef';
           textInputContainer[0].style.cursor = 'not-allowed';
         } 
-        // else { 
-        //   textInputContainer[0].style.backgroundColor = 'white';
-        //   textInputContainer[0].style.cursor = '';
-        // }
-
+       
       })
       .catch((error) => {
         console.error("Error:", error);
