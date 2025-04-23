71777e0800ad63cc7f60c8e57858cd2dabca28d0
Markus <marktayl1@users.noreply.github.com>
2025-04-23 14:54:30 -0700

    RAI fixes

--- src/backend/event_utils.py
+++ src/backend/event_utils.py
@@ -7,5 +7,5 @@ def track_event_if_configured(event_name: str, event_data: dict):
     instrumentation_key = os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING")
     if instrumentation_key:
         track_event(event_name, event_data)
-    else:
-        logging.warning(f"Skipping track_event for {event_name} as Application Insights is not configured")
+    # else:
+    #     logging.warning(f"Skipping track_event for {event_name} as Application Insights is not configured")
--- src/backend/utils_kernel.py
+++ src/backend/utils_kernel.py
@@ -155,7 +155,7 @@ async def rai_success(description: str) -> bool:
         
         CHECK_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
         API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
-        DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME")
+        DEPLOYMENT_NAME = os.getenv("AZURE_OPENAI_MODEL_NAME")
         
         if not all([CHECK_ENDPOINT, API_VERSION, DEPLOYMENT_NAME]):
             logging.error("Missing required environment variables for RAI check")
@@ -189,19 +189,20 @@ async def rai_success(description: str) -> bool:
         
         # Send request
         response = requests.post(url, headers=headers, json=payload, timeout=30)
-        response.raise_for_status()  # Raise exception for non-200 status codes
-        response_json = response.json()
-        
-        if (
-            response_json.get("choices")
-            and "message" in response_json["choices"][0]
-            and "content" in response_json["choices"][0]["message"]
-            and response_json["choices"][0]["message"]["content"] == "FALSE"
-            or response_json.get("error")
-            and response_json["error"]["code"] != "content_filter"
-        ):
-            return True
-        return False
+        if response.status_code == 400 or response.status_code == 200:
+            response_json = response.json()
+            
+            if (
+                response_json.get("choices")
+                and "message" in response_json["choices"][0]
+                and "content" in response_json["choices"][0]["message"]
+                and response_json["choices"][0]["message"]["content"] == "TRUE"
+                or response_json.get("error")
+                and response_json["error"]["code"] == "content_filter"
+            ):
+                return False
+        response.raise_for_status()  # Raise exception for non-200 status codes including 400 but not content_filter
+        return True
         
     except Exception as e:
         logging.error(f"Error in RAI check: {str(e)}")
